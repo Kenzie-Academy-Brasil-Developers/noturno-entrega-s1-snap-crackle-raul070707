58eb0bf7d070f07af8bedcdf0576da3e1907e378
Raul Ribeiro <raul260302@gmail.com>
2021-10-20 22:04:51 -0300

    resolvido a entrega 43

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,1 +1,20 @@
-// Seu código vai aqui
+function snapCrackle(maxValue){
+    let resultado = []
+    for(counter = 1; counter <= maxValue; counter++){
+        if (counter % 2 !== 0 && counter % 5 === 0){
+            resultado.push('SnapCrackle');
+        } else if (counter % 2 !== 0){
+            resultado.push('Snap');
+        } else if (counter % 5 === 0){
+            resultado.push('Crackle');
+        } else {
+            resultado.push(counter);
+        }
+    }
+    return resultado.join(', ')
+}
+console.log(snapCrackle(30));
+
+
+
+
