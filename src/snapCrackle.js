6ed0c3384ae3d45081cbf8c8588e7308d9605dbe
Raul Ribeiro <raul260302@gmail.com>
2021-10-25 07:38:56 -0300

    feito a tarefa bonus

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,4 +1,4 @@
-function snapCrackle(maxValue){
+/* function snapCrackle(maxValue){
     let resultado = []
     for(counter = 1; counter <= maxValue; counter++){
         if (counter % 2 !== 0 && counter % 5 === 0){
@@ -14,7 +14,41 @@ function snapCrackle(maxValue){
     return resultado.join(', ')
 }
 console.log(snapCrackle(30));
+ */
 
 
-
-
+function snapCracklePrime(maxValue){
+    let resultado = []
+    for(counter = 1; counter <= maxValue; counter++){
+        if (counter % 2 !== 0 && counter % 5 === 0){
+            if (counter / 5 === 1){
+                resultado.push('SnapCracklePrime');
+            } else {
+            resultado.push('SnapCrackle');
+            }
+        } else if (counter % 2 !== 0){
+            let array = []
+            for(let subcounter = 2; subcounter < counter; subcounter++){
+                if (counter % subcounter === 0){
+                    array.push(counter * -counter);
+                } else {
+                    array.push(counter);
+                }
+            }
+            let total = array.reduce((valor1, valor2)=> valor1 + valor2, 0)
+            if (total < 0){
+                resultado.push('Snap');
+            } else {
+                resultado.push('SnapPrime');
+            }
+            } else if (counter % 5 === 0){
+            resultado.push('Crackle');
+        } else if (counter / 2 === 1){
+            resultado.push('Prime');
+        } else {
+            resultado.push(counter);
+        }
+    }
+    return resultado.join(', ')
+}
+console.log(snapCracklePrime(30));
